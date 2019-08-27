cee0cd9c13c48933d0b42d55e874a65e4da22f85
igor <igordot@users.noreply.github.com>
2019-08-27 10:50:25 -0400

    update salmon

--- segments/quant-salmon.sh
+++ segments/quant-salmon.sh
@@ -100,7 +100,7 @@ fi
 
 # Salmon
 
-salmon_bin="/gpfs/data/igorlab/software/Salmon/salmon-0.13.1_linux_x86_64/bin/salmon"
+salmon_bin="/gpfs/data/igorlab/software/Salmon/salmon-0.14.1_linux_x86_64/bin/salmon"
 
 #  -l [ --libType ] arg       Format string describing the library type
 #  -i [ --index ] arg         salmon index
