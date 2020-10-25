43f802a84f126a895e17dcd7e2155ea26d43a974
igor <6363505+igordot@users.noreply.github.com>
2020-10-25 10:00:57 -0400

    adjust qc-coverage-gatk java mem

--- segments/qc-coverage-gatk.sh
+++ segments/qc-coverage-gatk.sh
@@ -102,7 +102,7 @@ fi
 # command
 # this segment failed for canFam3 WES (1.1M targets) with error "adjust the maximum heap size provided to Java"
 gatk_jar="/gpfs/data/igorlab/software/GenomeAnalysisTK/GenomeAnalysisTK-3.8-1/GenomeAnalysisTK.jar"
-gatk_cmd="java -Xms8G -Xmx8G -jar ${gatk_jar}"
+gatk_cmd="java -Xms32G -Xmx32G -jar ${gatk_jar}"
 
 if [ ! -s "$gatk_jar" ] ; then
 	echo -e "\n $script_name ERROR: GATK $gatk_jar DOES NOT EXIST \n" >&2
