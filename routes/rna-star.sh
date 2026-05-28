109b3367520cff85e2d2329cabab31056afc1edf
igor <6363505+igordot@users.noreply.github.com>
2026-05-28 14:46:34 -0400

    Adjust sbatch job partitions

--- routes/rna-star.sh
+++ routes/rna-star.sh
@@ -99,7 +99,7 @@ fi
 # generate BigWig (deeptools)
 segment_bw_deeptools="bigwig-deeptools"
 bash_cmd="bash ${code_dir}/segments/${segment_bw_deeptools}.sh $proj_dir $sample 4 $bam_star"
-sbatch_perf="--nodes=1 --ntasks=1 --cpus-per-task=5 --mem=25G"
+sbatch_perf="--nodes=1 --ntasks=1 --cpus-per-task=5 --mem=25G --partition=cpu_short,fn_short,gpu4_short"
 sbatch_mail="--mail-user=${USER}@nyulangone.org --mail-type=FAIL,REQUEUE"
 sbatch_name="--job-name=sns.${segment_bw_deeptools}.${sample}"
 sbatch_cmd="sbatch --time=8:00:00 ${sbatch_name} ${sbatch_perf} ${sbatch_mail} --export=NONE --wrap='${bash_cmd}'"
